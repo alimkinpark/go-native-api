dd1f1eb78f14b76345f378c89e1b21a067be484b
liam <alimalim9719@gmail.com>
2026-04-21 13:58:23 +0700

    mysql database

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"github.com/gorilla/mux"
-	"net/http"
+	"log"
+	"time"
 )
+import _ "github.com/go-sql-driver/mysql"
 
 func main() {
 	//http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -17,15 +19,122 @@ func main() {
 	//
 	//http.ListenAndServe(":80", nil)
 
-	r := mux.NewRouter()
+	//r := mux.NewRouter()
+	//
+	//r.HandleFunc("/books/{title}/page/{page}", func(writer http.ResponseWriter, request *http.Request) {
+	//	vars := mux.Vars(request)
+	//	title := vars["title"]
+	//	page := vars["page"]
+	//
+	//	fmt.Fprintf(writer, "You've requested the book: %s on page %s\n", title, page)
+	//})
+	//
+	//http.ListenAndServe(":80", r)
 
-	r.HandleFunc("/books/{title}/page/{page}", func(writer http.ResponseWriter, request *http.Request) {
-		vars := mux.Vars(request)
-		title := vars["title"]
-		page := vars["page"]
+	db, err := sql.Open("mysql", "root:@(127.0.0.1:3306)/belajar?parseTime=true")
 
-		fmt.Fprintf(writer, "You've requested the book: %s on page %s\n", title, page)
-	})
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	http.ListenAndServe(":80", r)
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	{
+		// create new DB
+
+		query := `
+				CREATE TABLE users (
+					id INT AUTO_INCREMENT,
+					username TEXT NOT NULL,
+					password TEXT NOT NULL,
+					created_at DATETIME,
+					PRIMARY KEY (id)
+				);`
+
+		if _, err := db.Exec(query); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	{
+		//	insert new users
+		username := "alimkin.park"
+		password := "rahasia"
+		createdAt := time.Now()
+
+		result, err := db.Exec("INSERT INTO users (username, password, created_at) VALUES (?,?,?)", username, password, createdAt)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		id, err := result.LastInsertId()
+
+		fmt.Println(id)
+	}
+
+	{
+		//	query single row / a single user
+
+		var (
+			id        int
+			username  string
+			password  string
+			createdAt time.Time
+		)
+
+		query := "SELECT id, username, password, created_at FROM users where id = ?"
+
+		if err := db.QueryRow(query, 1).Scan(&id, &username, &password, &createdAt); err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(id, username, password, createdAt)
+	}
+
+	{
+		//	query all users
+
+		type user struct {
+			id        int
+			username  string
+			password  string
+			createdAt time.Time
+		}
+
+		rows, err := db.Query("SELECT id, username, password, created_at FROM users")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+
+		var users []user
+
+		for rows.Next() {
+			var u user
+
+			if err := rows.Scan(&u.id, &u.username, &u.password, &u.createdAt); err != nil {
+				log.Fatal(err)
+			}
+
+			users = append(users, u)
+		}
+
+		if err := rows.Err(); err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%#v", users)
+	}
+
+	{
+		//	delete user
+
+		_, err := db.Exec("DELETE FROM users WHERE id = ?", 1)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
